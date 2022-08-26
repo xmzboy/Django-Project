cdbe861a96ce8cb0315a0507ffda2a0d931ad670
Ivan Andreichuk <vanichpz@gmail.com>
2022-08-26 17:37:08 +0300

    Update urls.py

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics')
 ]
