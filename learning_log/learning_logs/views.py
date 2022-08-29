0ea56f82777d7d44c888dd9a174ce07d20a5ceed
Ivan Andreichuk <vanichpz@gmail.com>
2022-08-29 20:01:00 +0300

    Update views.py
    Add protect to pages

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 
 def index(request):
@@ -10,7 +11,7 @@ def index(request):
 
 @login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -18,6 +19,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic.owner, request.user)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -30,7 +32,9 @@ def new_topic(request):
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
@@ -39,6 +43,7 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic.owner, request.user)
     if request.method != 'POST':
         form = EntryForm()
     else:
@@ -57,7 +62,7 @@ def new_entry(request, topic_id):
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-
+    check_topic_owner(topic.owner, request.user)
     if request.method != 'POST':
         form = EntryForm(instance=entry)
     else:
@@ -68,3 +73,8 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+
+def check_topic_owner(topic_owner, request_user):
+    if topic_owner != request_user:
+        raise Http404
