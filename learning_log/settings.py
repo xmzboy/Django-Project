76209b6df90e453f7f32382505e61016c5f27fc4
Ivan Andreichuk <vanichpz@gmail.com>
2022-09-02 12:30:57 +0300

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,11 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# with open('learning_log/secret.txt', 'r') as f:
-#     SECRET_KEY = f.read()
+
 SECRET_KEY = 'django-insecure-7xyr=ltl#1%i*sapf0ej-ez*zu5a^ae)y)k&jn*m3c^gjgy&d4'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
